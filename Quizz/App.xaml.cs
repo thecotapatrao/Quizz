ac7f9baed26c896671fdaf06a74ff6aa8f5b394a
thecotapatrao <abdulaidjibril@gmail.com>
2024-11-15 20:53:54 +0000

    Adicionar arquivos de projeto.

---
+++ Quizz/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Quizz
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
